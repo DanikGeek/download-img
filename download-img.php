abd8d01d9ed5f477ee7a23be0ecbafdb0cd1beb3
DanikGeek <53581368+DanikGeek@users.noreply.github.com>
2023-05-05 12:53:13 +0600

    Улучшенный
    Теперь не скачивает несуществующие картинки

--- download-img.php
+++ download-img.php
@@ -4,7 +4,7 @@
 $imageUrls = array(
     'https://images.satu.kz/182234695_udobrenie-ph-perfect.jpg',
     'https://images.satu.kz/180056163_udobrenie-ph-perfect.jpg',
-	'https://images.satu.kz/180056165675_udobrenie-ph-perfet.jpg',
+    'https://images.satu.kz/180056165675_udobrenie-ph-perfet.jpg',
     'https://images.satu.kz/191059352_udobrenie-ph-perfect.jpg',
 );
 
@@ -24,33 +24,36 @@ foreach ($imageUrls as $imageUrl) {
     // Определяем путь к файлу на сервере
     $filePath = $folder . $fileName;
 
-    // Открываем файл для записи
-    $file = fopen($filePath, 'w');
+    // Проверяем, существует ли изображение
+    $headers = @get_headers($imageUrl);
+    if ($headers && strpos($headers[0], '200')) {
+        // Изображение существует, продолжаем скачивание
 
-    // Инициализируем cURL-сессию
-    $curl = curl_init($imageUrl);
+        // Открываем файл для записи
+        $file = fopen($filePath, 'w');
 
-    // Устанавливаем параметры cURL-сессии
-    curl_setopt($curl, CURLOPT_FILE, $file);
-    curl_setopt($curl, CURLOPT_HEADER, 0);
+        // Инициализируем cURL-сессию
+        $curl = curl_init($imageUrl);
 
-    // Выполняем запрос на скачивание изображения
-    curl_exec($curl);
+        // Устанавливаем параметры cURL-сессии
+        curl_setopt($curl, CURLOPT_FILE, $file);
+        curl_setopt($curl, CURLOPT_HEADER, 0);
 
-    // Проверяем код ответа на предмет ошибок
-    $statusCode = curl_getinfo($curl, CURLINFO_HTTP_CODE);
-    if ($statusCode != 200) {
-        echo "<br><br> <span style='color:red;'>Не удалось скачать изображение</span> $imageUrl (код ответа $statusCode)\n";
+        // Выполняем запрос на скачивание изображения
+        curl_exec($curl);
+
+        // Закрываем cURL-сессию
+        curl_close($curl);
+
+        // Закрываем файл
+        fclose($file);
+
+        // Выводим сообщение об успешном скачивании
+        echo "<br><br>Изображение $imageUrl успешно скачано и сохранено в $filePath\n";
+    } else {
+        // Изображение не существует, выводим сообщение об ошибке
+        echo "<br><br> <span style='color:red;'>Не удалось скачать изображение</span> $imageUrl\n";
     }
-
-    // Закрываем cURL-сессию
-    curl_close($curl);
-
-    // Закрываем файл
-    fclose($file);
-
-    // Выводим сообщение об успешном скачивании
-    echo "<br><br>Изображение $imageUrl успешно скачано и сохранено в $filePath\n";
 }
-Проверка версии
+
 ?>
